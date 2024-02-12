55ad5f6eb9a1cebf67a9bb38c58aa243b4ebae62
Kalinina Karina <kalinina.karina10@yandex.ru.ru>
2024-02-12 23:06:55 +0300

    upd

--- src/test/java/api/reqres/specifications/Specifications.java
+++ src/test/java/api/reqres/specifications/Specifications.java
@@ -1,8 +1,10 @@
 package api.reqres.specifications;
 
+import io.qameta.allure.restassured.AllureRestAssured;
 import io.restassured.RestAssured;
 import io.restassured.builder.RequestSpecBuilder;
 import io.restassured.builder.ResponseSpecBuilder;
+import io.restassured.filter.log.LogDetail;
 import io.restassured.http.ContentType;
 import io.restassured.specification.RequestSpecification;
 import io.restassured.specification.ResponseSpecification;
@@ -12,35 +14,42 @@ public class Specifications {
         return new RequestSpecBuilder()
                 .setBaseUri(url)
                 .setContentType(ContentType.JSON)
+                .log(LogDetail.ALL)
+                .addFilter(new AllureRestAssured())
                 .build();
     }
 
     public static ResponseSpecification responseSpecOK200(){
         return new ResponseSpecBuilder()
+                .log(LogDetail.ALL)
                 .expectStatusCode(200)
                 .build();
     }
 
     public static ResponseSpecification responseSpecOK201(){
         return new ResponseSpecBuilder()
+                .log(LogDetail.ALL)
                 .expectStatusCode(201)
                 .build();
     }
 
     public static ResponseSpecification responseSpecOK204(){
         return new ResponseSpecBuilder()
+                .log(LogDetail.ALL)
                 .expectStatusCode(204)
                 .build();
     }
 
     public static ResponseSpecification responseSpecERROR400(){
         return new ResponseSpecBuilder()
+                .log(LogDetail.ALL)
                 .expectStatusCode(400)
                 .build();
     }
 
     public static ResponseSpecification responseSpecNOTFOUND404(){
         return new ResponseSpecBuilder()
+                .log(LogDetail.ALL)
                 .expectStatusCode(404)
                 .build();
     }
