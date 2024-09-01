82bb54d921357a31fd13fc9814fffbc5fcebd2d6
Kalinina Karina <kalinina.karina10@yandex.ru.ru>
2024-09-01 21:27:32 +0300

    upd test1

--- jenkinsfile.groovy
+++ jenkinsfile.groovy
@@ -63,7 +63,7 @@ def getProject(String repo, String branch) {
 
 def generateAllure() {
     allure([
-            includeProperties: false,
+            includeProperties: true,
             jdk              : '',
             properties       : [],
             reportBuildPolicy: 'ALWAYS',
