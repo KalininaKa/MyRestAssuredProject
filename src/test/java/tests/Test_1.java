8710584fcba6bff78f55b583f7d644644eced219
Kalinina Karina <kalinina.karina10@yandex.ru.ru>
2024-09-01 18:50:58 +0300

    upd test1

--- src/test/java/tests/Test_1.java
+++ src/test/java/tests/Test_1.java
@@ -12,16 +12,15 @@ import java.util.List;
 import static io.restassured.RestAssured.given;
 
 @Tag("@API")
+@Link(name = "Ссылка на reqres.in", url = "https://reqres.in")
+@Owner(value = "Калинина Карина Андреевна")
+@Severity(value = SeverityLevel.CRITICAL)
+@Issue(value = "UT-4627")
 @DisplayName("(RecresTest_1) Аватары содержат айди пользователей")
 public class Test_1 {
     private static String URL = "https://reqres.in/";
 
     @Test
-    @Description(value = "(RecresTest_1) Аватары содержат айди пользователей")
-    @Link(name = "Ссылка на reqres.in", url = "https://reqres.in")
-    @Owner(value = "Калинина Карина Андреевна")
-    @Severity(value = SeverityLevel.CRITICAL)
-    @Issue(value = "UT-4627")
     public void checkAvatarAndIdTest() {
         Specifications.installSpecification(Specifications.requestSpec(URL), Specifications.responseSpecOK200());
 
